4356b1ce343f8ca2db99c3cb9f21fb73022b977f
utkudarilmaz <utkudarilmaz@gmail.com>
2018-10-28 22:02:07 +0300

    Updated Namespace

--- honaitower.py
+++ honaitower.py
@@ -17,23 +17,23 @@ class HonaiTower():
 
     def __init__(self,size):
 
-        self.sutunlar = []
+        self.Stacks = []
         self.size = size
         for i in range(3):
-            self.sutunlar.append(Sutun(i))
+            self.Stacks.append(Stack(i))
 
         for i in range(size,0,-1):
-            self.sutunlar[0].disks.append(Disk(i))
+            self.Stacks[0].disks.append(Disk(i))
 
         self.start()
 
     def get_tower(self):
 
         for i in range(3):
-            if self.sutunlar[i].is_empty():
+            if self.Stacks[i].is_empty():
                 continue
             else :
-                self.sutunlar[i].get_sutun()
+                self.Stacks[i].get_Stack()
 
     def start(self):
         self.get_tower()
@@ -43,28 +43,28 @@ class HonaiTower():
 
         print(colors.HEADER+"++++++++++++++++++++++++++++++++++++++++++++++++"+colors.ENDC)
 
-        source = int(input(colors.OKBLUE+"Hangi sutundaki diski kaydirmak istiyorsunuz: "+colors.ENDC))
+        source = int(input(colors.OKBLUE+"Hangi Stackdaki diski kaydirmak istiyorsunuz: "+colors.ENDC))
         if source > 2 or source < 0:
-            print(colors.FAIL+"Hata: Yanlıs sutun secimi"+ colors.ENDC)
+            print(colors.FAIL+"Hata: Yanlıs Stack secimi"+ colors.ENDC)
             return self.move_disk()
 
-        if self.sutunlar[source].is_empty() :
-            print(colors.FAIL+"Hata: Kaynak sutunda disk yok"+ colors.ENDC)
+        if self.Stacks[source].is_empty() :
+            print(colors.FAIL+"Hata: Kaynak Stackda disk yok"+ colors.ENDC)
             return self.move_disk()
 
-        target = int(input(colors.OKBLUE+"Hangi sutuna kaydirmak istiyorsunuz: "+colors.ENDC))
+        target = int(input(colors.OKBLUE+"Hangi Stacka kaydirmak istiyorsunuz: "+colors.ENDC))
 
         if target > 2 or target < 0:
-            print(colors.FAIL+"Hata: Yanlıs sutun secimi"+ colors.ENDC)
+            print(colors.FAIL+"Hata: Yanlıs Stack secimi"+ colors.ENDC)
             return self.move_disk()
 
-        if self.sutunlar[target].is_empty():
-            self.sutunlar[target].disks.append(self.sutunlar[source].disks.pop())
-        elif self.sutunlar[source].top()>self.sutunlar[target].top():
-            print(colors.FAIL+"Hata: Yer değiştirdiğiniz disk hedef sutundaki diskten kucuk olmali!"+ colors.ENDC)
+        if self.Stacks[target].is_empty():
+            self.Stacks[target].disks.append(self.Stacks[source].disks.pop())
+        elif self.Stacks[source].top()>self.Stacks[target].top():
+            print(colors.FAIL+"Hata: Yer değiştirdiğiniz disk hedef stackdaki diskten kucuk olmali!"+ colors.ENDC)
             return self.move_disk()
         else :
-            self.sutunlar[target].disks.append(self.sutunlar[source].disks.pop())
+            self.Stacks[target].disks.append(self.Stacks[source].disks.pop())
 
         self.is_win()
         os.system('clear')
@@ -74,14 +74,14 @@ class HonaiTower():
 
     def is_win(self):
         print('QQQQ')
-        if self.sutunlar[0].is_empty() and self.sutunlar[1].is_empty() and self.sutunlar[2].get_size() == self.size:
+        if self.Stacks[0].is_empty() and self.Stacks[1].is_empty() and self.Stacks[2].get_size() == self.size:
             print('YYYYY')
             os.system('clear')
             print(colors.HEADER+"Tebrikler! Kazandiniz"+colors.ENDC)
             self.get_tower()
             exit()
 
-class Sutun():
+class Stack():
 
     def __init__(self,name):
         self.name = str(name)
@@ -90,7 +90,7 @@ class Sutun():
 
     def __str__(self):
 
-        return self.name + ". Sutun"
+        return self.name + ". Stack"
 
     def get_size(self):
         return len(self.disks)
@@ -101,7 +101,7 @@ class Sutun():
     def top(self):
         return int(self.disks[self.get_size()-1].weight)
 
-    def get_sutun(self):
+    def get_Stack(self):
 
         s = self.get_size() - 1
         while s >= 0:
